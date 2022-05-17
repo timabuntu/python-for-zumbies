7d4ff9f749dbf1be5bc0fa287b6aa4b2e4cef3e5
thiago lopes de mello <timabuntu@gmail.com>
2022-05-17 16:16:57 -0300

    learn: getting data by input number float

--- data-input/table_test.py
+++ data-input/table_test.py
@@ -3,6 +3,11 @@
 
 # nome = input('Digite seu nome: ')
 
-numberInput1 = int(input('n1: '))
-numberInput2 = int(input('n2: '))
+# numberInput1 = int(input('n1: '))
+# numberInput2 = int(input('n2: '))
+# print(numberInput1 + numberInput2)
+
+
+numberInput1 = float(input('N1:'))
+numberInput2 = float(input('N2:'))
 print(numberInput1 + numberInput2)
