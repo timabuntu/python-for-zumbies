a6452b10e13e694861f1031736d5482e27b2ea52
thiago lopes de mello <timabuntu@gmail.com>
2022-05-16 21:07:56 -0300

    learn: operators

---
+++ first-variables/operators.py
@@ -0,0 +1,7 @@
+print("soma", 3 + 4)
+print("multiplica", 12 * 5)
+print("exponenciação", 2 ** 10)
+print("divisão", 10 / 3)
+print()
+#! Atenção: algumas linguagens a divisão de 1 / 2 da zero, no python retorna float
+print("divisão", 1 / 2)
